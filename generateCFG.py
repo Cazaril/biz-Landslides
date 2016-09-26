395ffaef32c2ed1f37772dcca4bdb9d1055dfb34
egonzalo <egonzalo@conwet.com>
2016-09-26 13:50:08 +0200

    Unformated regexp hell

--- generateCFG.py
+++ generateCFG.py
@@ -36,17 +36,42 @@ names = [{'name': 'User-Guide'},
 
 # Y con esta pretendo splitear las slides en las imagenes
 # .. \w*:: (\/.*)\n.*
-with open('./manualTemplate.txt') as template: #, open('./pruebita.txt', 'w+') as g:
-    file = template.read()
-    formatedFile = re.sub(r'(.*\n)(=+|-+)', r'\2\n\1', file)
-#    g.write(formatedFile)
+
+# Las notas de las imagenes van a ser un poco mas putada, voy a probar a poner
+# cada nota con su imagen, pero no creo que quede bien
+# (\.\. note::\n)(.*)
+with open('./manualTemplate.txt') as template:
+    temp = template.read()
+    formatedFile = re.sub(r'(.*\n)(=+|-+)', r'\2\n#\1', temp)
     l = re.split(r'=+|-+', formatedFile)
     i = 1
     for nam in names:
-        with open('./{}.md'.format(nam.get('name')), 'w') as g:
-            g.write('# Introduction to WireCloud\n')
-            g.write('.fx: cover\n')
-            g.write('@conwet\n')
-            g.write('---\n')
+        with open('./{}.text'.format(nam.get('name')), 'w') as g:
             g.write('{}\n'.format(l[i]))
             i = i + 1
+
+for nam in names:
+    with open('./{}.text'.format(nam.get('name')), 'r') as g:
+        fich = g.read()
+        f1Text = re.sub(r'(\.\. \w*:: )(\/.*)\n(.*)\n\n(\.\. note::\n\s*)(.*)',
+                        r'---\n#{}\n![\5](.\2)\n---\n#{}\n'.format(nam.get('name'),
+                                                                   nam.get('name')),
+                        fich)
+        f1Text = re.sub(r'(\.\. \w*:: )(\/.*)\n(.*)\n\n(?!\.\.)',
+                        r'---\n#{}\n![](.\2)\n---\n'.format(nam.get('name')),
+                        f1Text)
+        formatedText = ''
+        while(formatedText != f1Text):
+            formatedText = re.sub(r'(\.\. \w*:: )(\/.*)\n(.*)\n\n(?!\.\.)',
+                                  r'---\n#{}\n![](.\2)\n'.format(nam.get('name')),
+                                  f1Text)
+            aux = formatedText
+            formatedText = f1Text
+            f1Text = aux
+
+        with open('./{}.md'.format(nam.get('name')), 'w') as md:
+            md.write('# Introduction to WireCloud\n')
+            md.write('.fx: cover\n')
+            md.write('@conwet\n')
+            md.write('---\n')
+            md.write(formatedText)
